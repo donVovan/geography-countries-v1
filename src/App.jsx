d8dda668edaf0d80693bf7353b0d67ce5ee9c7aa
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-10-03 16:50:27 +0300

    Finished Menu

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,35 @@ function App() {
                         </a>
                     </div>
                 </header>
+                <main className="main">
+                    <div className="sidebar">
+                        <div className="menu">
+                            <ul>
+                                <li><a href={"/"}>Европа</a></li>
+                                <li><a href={"/"}>Азия</a></li>
+                                <li><a href={"/"}>Северная Америка</a></li>
+                                <li><a href={"/"}>Южная Америка</a></li>
+                                <li><a href={"/"}>Африка</a></li>
+                                <li><a href={"/"}>Австралия и Океания</a></li>
+                            </ul>
+                        </div>
+                        <div className="sidebar_box">
+                            <h3>Подзаголовок</h3>
+                            <div className="side_box">что-то здесь</div>
+                            <div className="side_box">и здесь...</div>
+                            <div className="side_box">...и здесь</div>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <h1>самый главный заголовок</h1>
+                        <p>какой-то текст</p>
+                    </div>
+                </main>
+                <footer className="footer">
+                    <p><a href={"/"}>какая-то ссылка</a></p>
+                </footer>
 
-                <p>какой-то текст</p>
-                <p><a href={"/"}>какая-то ссылка</a></p>
+
             </div>
         </div>
     )
