3e5c2f00fff7ad544fe2affcb4df2ff88aaf522b
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-11-03 14:06:34 +0300

    Connect to db v.6

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -24,7 +24,7 @@ function Container() {
     }, []);
 
     async function fetchData() {
-        const response = await fetch('http://localhost:80/');
+        const response = await fetch('http://localhost:27017/');
         const data = await response.json();
         setJsonData(data);
     }
